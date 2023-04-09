c38acca1b3df3c8b881b2d4c5bc6eb1bcc320d79
unknown <martinnavratilu@gmail.com>
2023-04-09 14:29:13 +0200

    added CLI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
-
+use std::env;
 mod parser;
 mod interpreter;
 
 fn main(){
-    println!("Gimmie the code:");
+    let arg1 = env::args().nth(1);
+
     let mut code:String= String::new();
-    std::io::stdin().read_line(&mut code).expect("No code provided");
+    match arg1{
+        Some(a)=>{
+           match a.as_str() {
+               "-dir" | "--dir" => {
+                    match env::args().nth(2) {
+                        Some(directory) => {
+                            println!("Reading data from directory: {}",directory);
+                            //TODO: loading data logic
+                        },
+                        None => {
+                            println!("Argument was not provided\nShutting down...");
+                            return;
+                        },
+                    }
+                },
+               "-h" | "--help" => {
+                    println!("Manual:");
+                    println!("--help\t-h\tto get help");
+                    println!("--dir\t-dir\tYou have to specify directory");
+                    println!("\t\t-dir [directory]");
+                    return;
+                },
+                _=>println!("Error:Invalid parameter\nTry:\n\t--help to get help"),
+           } 
+
+        },
+        _=>{
+            println!("Gimmie the code:");
+            std::io::stdin().read_line(&mut code).expect("No code provided");
+            }
+    }
 
     let parsed = parser::parse(code);
     interpreter::run(parsed);
+    
+    
 }
